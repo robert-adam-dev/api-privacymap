610b8fe24dd512c8aadf6569703f6f280cbef98b
Robert Adam <r.neves1992@gmail.com>
2021-04-06 16:12:34 -0300

    added documentation

--- src/main/java/co/privacymap/api/config/security/SecurityConfigurations.java
+++ src/main/java/co/privacymap/api/config/security/SecurityConfigurations.java
@@ -55,6 +55,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Override // Configura recursos estáticos
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 
 }
---
+++ src/main/java/co/privacymap/api/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,38 @@
+package co.privacymap.api.config.swagger;
+
+import co.privacymap.api.model.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfigurations {
+
+    @Bean
+    public Docket privacymapApi(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("co.privacymap.api"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .ignoredParameterTypes(Client.class)
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Header for token JWT")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(false)
+                        .build()
+                ));
+
+    }
+}
